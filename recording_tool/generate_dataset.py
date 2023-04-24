d2cb727850883652a50cb4ab3168ac41b8dc8b0a
WANZL\dankem <daniel.kempfle@wanzl.com>
2023-04-24 07:39:43 +0200

    take random 10% from train to test and val

--- recording_tool/generate_dataset.py
+++ recording_tool/generate_dataset.py
@@ -2,43 +2,51 @@ import os,os.path
 import shutil
 import glob
 from natsort import natsorted
+import random
 
 dir_tool = os.getcwd() + f"\\recording_tool" 
 dir_mm = "multiple_manipulated"
 dir_mp = "manipulated_images"
+choice = ""
 
 multiple = input('Type "m" for multiple: ') == "m"
-vid = input('Type which video: ')
 product = input ('Type the product number: ')
+vid = input('Type which video: ')
 if multiple:
     recording_tool = glob.glob(f'{dir_tool}\\{dir_mm}\\{vid}\\{product}\\**\\')
+    choice = "multi"
 else:
     recording_tool = glob.glob(f'{dir_tool}\\{dir_mp}\\{vid}\\{product}\\**\\')
+    choice = "single"
 
 destination_folder = f'{dir_tool}\\'
 png = 'png'
 txt = 'txt'
 images = 'images\\'
 labels = 'labels\\'
+dataset = 'datasets\\'
+dataset_path = destination_folder + dataset + product + "_" + vid + "_" + choice
 
 train = 'train\\'
 val = 'val\\'
 test = 'test\\'
 
-if os.path.exists(destination_folder + train):
-    ""
-else:
-    os.mkdir(destination_folder + train)
-    os.mkdir(destination_folder + train + images)
-    os.mkdir(destination_folder + train + labels)
+if not os.path.exists(destination_folder + dataset):
+    os.mkdir(destination_folder + dataset)
 
-    os.mkdir(destination_folder + val)
-    os.mkdir(destination_folder + val + images)
-    os.mkdir(destination_folder + val + labels)
+os.mkdir(dataset_path)
 
-    os.mkdir(destination_folder + test)
-    os.mkdir(destination_folder + test + images)
-    os.mkdir(destination_folder + test + labels)
+os.mkdir(destination_folder + train)
+os.mkdir(destination_folder + train + images)
+os.mkdir(destination_folder + train + labels)
+
+os.mkdir(destination_folder + val)
+os.mkdir(destination_folder + val + images)
+os.mkdir(destination_folder + val + labels)
+
+os.mkdir(destination_folder + test)
+os.mkdir(destination_folder + test + images)
+os.mkdir(destination_folder + test + labels)
 
 file_number = 0
 for folder in recording_tool:
@@ -56,8 +64,7 @@ count = 0
 for folder in recording_tool:
     natsorted(folder)
     for file in os.listdir(folder):
-        if train_percentage > 0:
-        #if train_percentage > 0 and count%10 != 0 and count%11 != 0: 
+        if png_or_txt > 0:
             source = folder + file
 
             if png in source:
@@ -69,37 +76,62 @@ for folder in recording_tool:
                 if os.path.isfile(source):
                     destination = destination_folder + train + labels + file
                     shutil.copy(source, destination)
-                    train_percentage = train_percentage - 1
+                    png_or_txt -= 1
                     count += 1
 
-        elif val_percentage > 0: 
-        #elif count%10 == 0 and val_percentage > 0:
-            source = folder + file
+while  val_percentage > 0: 
+    for file in os.listdir(destination_folder + train + images):
+        rand_int = random.randint(0, 9)
+        filename = file.split(".")
 
-            if png in source:
-                if os.path.isfile(source):
-                    destination = destination_folder + val + images + file
-                    shutil.copy(source, destination)
-                                       
-            if txt in source:
-                if os.path.isfile(source):
-                    destination = destination_folder + val + labels + file
-                    shutil.copy(source, destination)
-                    val_percentage = val_percentage - 1
-                    count += 1
+        source_img = destination_folder + train + images + file
+        source_labels = destination_folder + train + labels + filename[0] + ".txt"
 
-        elif test_percentage > 0:
-        #elif count%11 == 0 and test_percentage > 0: 
-            source = folder + file
+        destination_img = destination_folder + val + images + file          
+        destination_labels = destination_folder + val + labels + filename[0] + ".txt"
 
-            if png in source:
-                if os.path.isfile(source):
-                    destination = destination_folder + test + images + file
-                    shutil.copy(source, destination)
-                    
-            if txt in source:
-                if os.path.isfile(source):
-                    destination = destination_folder + test + labels + file
-                    shutil.copy(source, destination)
-                    test_percentage = test_percentage - 1
-                    count += 1
+        if rand_int == 9:
+            if png in source_img:
+                if os.path.isfile(source_img):
+                    shutil.copy(source_img, destination_img)
+                    os.remove(destination_folder + train + images + file)
+                    val_percentage -= 1
+            
+            for txt_file in os.listdir(destination_folder + train + labels):
+                if txt_file == filename[0] + ".txt":
+                        shutil.copy(source_labels, destination_labels)
+                        os.remove(destination_folder + train + labels + filename[0] + ".txt")
+        
+        if val_percentage == 0:
+            break
+
+while test_percentage > 0:
+    for file in os.listdir(destination_folder + train + images):
+        rand_int = random.randint(0, 10)
+        filename = file.split(".")
+
+        source_img = destination_folder + train + images + file
+        source_labels = destination_folder + train + labels + filename[0] + ".txt"
+
+        destination_img = destination_folder + test + images + file
+        destination_labels = destination_folder + test + labels + filename[0] + ".txt"
+
+        if rand_int == 10:
+            if png in source_img:
+                if os.path.isfile(source_img):
+                    shutil.copy(source_img, destination_img)
+                    os.remove(destination_folder + train + images + file)
+                    test_percentage -= 1
+                
+            for txt_file in os.listdir(destination_folder + train + labels):
+                if txt_file == filename[0] + ".txt":
+                    shutil.copy(source_labels, destination_labels)
+                    os.remove(destination_folder + train + labels + filename[0] + ".txt")
+
+        if test_percentage == 0:
+            break
+
+
+shutil.move(destination_folder + train, dataset_path)
+shutil.move(destination_folder + val, dataset_path)
+shutil.move(destination_folder + test, dataset_path)
