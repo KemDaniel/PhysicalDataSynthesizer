64ef1f197638e1d133f7f48bef7f3f716d5b62d8
WANZL\dankem <daniel.kempfle@wanzl.com>
2023-04-26 08:39:33 +0200

    created folder structure for videos

--- recording_tool/save_video_frames.py
+++ recording_tool/save_video_frames.py
@@ -3,25 +3,33 @@ import os
 
 
 vid_number = input("Type the name of the video: ")
+vid_path = os.getcwd() + f"\\recording_tool\\videos\\"
 
-frames = os.getcwd() + f"\\recording_tool\\manipulated_images\\{vid_number}" 
+all_vid = vid_path + f"all_vid\\"
+
+frames = os.getcwd() + f"\\recording_tool\\videos\\{vid_number}" 
 create_frames = os.path.join(frames, "frames")
 
-if os.path.exists(create_frames):
-     ""
-else:
+if not os.path.exists(vid_path):
+     os.mkdir(vid_path)
+if not os.path.exists(all_vid):
+     os.mkdir(all_vid)   
+if not os.path.exists(frames):
+     os.mkdir(frames)
+if not os.path.exists(create_frames):
      os.mkdir(create_frames)
-
-vidcap = cv2.VideoCapture(f"C:/Users/dankem/Pictures/Videos/{vid_number}.mp4")
+     
+vidcap = cv2.VideoCapture(all_vid + f"{vid_number}.mp4")
 success,image = vidcap.read()
 count = 0
 frame_count = 0
-while success:
-    success,image = vidcap.read()
-    print('Read a new frame: ', success)
-    if count % 25==0:
-        saved= cv2.imwrite(f"recording_tool/manipulated_images/{vid_number}/frames/frame%d.png" % frame_count, image) 
-        print('frame saved')
-        frame_count += 1
-    count += 1
-        
+if image is not None:
+     while success:
+          success,image = vidcap.read()
+          #print('Read a new frame: ', success)
+          if count % 25==0:
+               saved= cv2.imwrite(f"recording_tool/videos/{vid_number}/frames/frame%d.png" % frame_count, image) 
+               print('frame saved')
+               frame_count += 1
+          count += 1
+     
