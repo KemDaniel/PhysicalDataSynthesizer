84345a5e5c763a3910488966ec8488223903e5e4
WANZL\dankem <daniel.kempfle@wanzl.com>
2023-08-07 13:24:57 +0200

    scripts for final dataset with image processing

---
+++ recording_tool/exp8_final_dataset.py
@@ -0,0 +1,314 @@
+from PIL import Image, ImageFilter, ImageEnhance
+import random
+import os
+import glob
+from natsort import natsorted
+import datetime
+import numpy as np
+import copy
+from timm.data.random_erasing import RandomErasing
+from torchvision import transforms
+
+dir_tool = "recording_tool"
+dir_mp = "manipulated_images"
+dir_all_vid = "videos"
+dir_vid = "vid"
+dir_vid1 = "vid1"
+dir_vid2 = "vid2"
+dir_vid3 = "vid3"
+cam1 = "223"
+cam2 = "224"
+cam3 = "225"
+cam4 = "226"
+cam5 = "227"
+cam6 = "228"
+count = 0
+dataset = "origin"
+
+# real products
+#product_list = ["0700", "0701", "0702", "0703", "0704", "0705", "0706", "0707", "0708", "0709", "0710",
+#                "0720", "0721", "0722", "0723", "0724", "0725", "0726", "0727", "0728", "0729", "0730",
+#                "0740", "0741", "0742", "0743", "0744", "0745", "0746", "0747", "0748", "0749",
+#                "0750", "0751", "0752", "0753", "0754", "0755", "0756", "0757", "0758", "0759", 
+#                "0760", "0761", "0762", "0763", "0764", "0765", "0766", "0767", "0768", "0769",
+#                "0770", "0771", "0772", "0773", "0774", "0775", "0776"]
+# euroshop products
+product_list = ["0600", "0601", "0602", "0603", "0604", "0605", "0606", "0607", "0608", "0609", "0610", 
+                "0611", "0612", "0613", "0614", "0615", "0616", "0617", "0618", "0619", "0620", "0621", "0622"]
+
+vid_list = ["vid", "vid1", "vid2", "vid3"] 
+used_vid = []
+current_time = str(datetime.datetime.now().strftime("%H_%M"))
+
+vid_number = input("Tippe die Anzahl an Videos ein: ")
+img_manipulation = True
+manipulation = ["final", "origin"]                                                                                       ## add new manipulation
+
+parent_dir_mp = os.getcwd() + f"\\{dir_tool}" 
+create_dir_mp = os.path.join(parent_dir_mp, dir_mp)
+
+def create_directories():
+    # add new videos here and create it 
+    create_dir_vid = os.path.join(create_dir_mp, dir_vid)
+    create_dir_vid1 = os.path.join(create_dir_mp, dir_vid1)
+    create_dir_vid2 = os.path.join(create_dir_mp, dir_vid2)
+    create_dir_vid3 = os.path.join(create_dir_mp, dir_vid3)
+
+    for x in manipulation:
+        if not img_manipulation:
+            x = "origin"
+        dataset_name = os.path.join(create_dir_vid, x + "_" + current_time)
+
+        cam1_vid = os.path.join(dataset_name, cam1)
+        cam2_vid = os.path.join(dataset_name, cam2)
+        cam3_vid = os.path.join(dataset_name, cam3)
+        cam4_vid = os.path.join(dataset_name, cam4)
+        cam5_vid = os.path.join(dataset_name, cam5)
+        cam6_vid = os.path.join(dataset_name, cam6)
+
+        if os.path.exists(create_dir_mp):
+            print(f"\nDirectory '{dir_mp}' exists already.\n")
+        else:
+            os.mkdir(create_dir_mp)
+            print(f"\nDirectory '{dir_mp}' created.\n")     
+
+        if not os.path.exists(create_dir_vid):
+            os.mkdir(create_dir_vid)
+        if not os.path.exists(create_dir_vid1):
+            os.mkdir(create_dir_vid1)
+        if not os.path.exists(create_dir_vid2):
+            os.mkdir(create_dir_vid2)
+        if not os.path.exists(create_dir_vid3):
+            os.mkdir(create_dir_vid3)
+
+        if not os.path.exists(dataset_name):
+            os.mkdir(dataset_name)
+
+        if not os.path.exists(cam1_vid):
+            os.mkdir(cam1_vid)
+        if not os.path.exists(cam2_vid):
+            os.mkdir(cam2_vid)
+        if not os.path.exists(cam3_vid):
+            os.mkdir(cam3_vid)
+        if not os.path.exists(cam4_vid):
+            os.mkdir(cam4_vid)
+        if not os.path.exists(cam5_vid):
+            os.mkdir(cam5_vid)
+        if not os.path.exists(cam6_vid):
+            os.mkdir(cam6_vid)
+        
+
+def product_rotation():
+    random_rotate = random.randrange(0, 360)
+    random_x = random.randrange(129, 720)
+    if random_x > 500:
+        random_y = random.randrange(510, 720) 
+        if cam_nb[0] == "224" or cam_nb[0] == "225" or cam_nb[0] == "226":
+            random_size = round(random.uniform(0.5, 0.55),2)
+        else:
+            random_size = round(random.uniform(0.35, 0.4),2)
+    else:
+        random_y = random.randrange(400, 720)
+        if cam_nb[0] == "224" or cam_nb[0] == "225" or cam_nb[0] == "226":
+            random_size = round(random.uniform(0.65, 0.7),2)
+        else:
+            random_size = round(random.uniform(0.4, 0.5),2)
+        
+    img_resized = img.resize((round(img.size[0]*random_size), round(img.size[1]*random_size))) 
+    img_rotated = img_resized.rotate(random_rotate, expand=True)
+
+    cropped_img_rotated = img_rotated.crop(img_rotated.getbbox())
+
+    return cropped_img_rotated, random_x, random_y
+
+def blur(blurred_list):
+    
+    ## 80% blurred products
+
+    if random.randint(1,100) > 20:
+        blurredImage = cropped_img_rotated.filter(ImageFilter.GaussianBlur(random.randint(1,3)))
+    else: 
+        blurredImage = cropped_img_rotated
+
+    blurred_list.append(blurredImage)
+
+    brightness(blurredImage)
+
+    return blurred_list
+
+
+def brightness(blurredImage):
+
+    ## every product receives a brigthness of 0.5 to 1.5
+
+    brightImage = blurredImage.copy()
+
+    enhancer = ImageEnhance.Brightness(brightImage)
+
+    brightness = round(random.uniform(0.5, 1.5),1)
+
+    brightImage = enhancer.enhance(brightness)
+
+    brightness_list.append(brightImage)        
+
+    colorSpace(brightImage)
+
+    return brightness_list
+
+
+def colorSpace(brightImage):
+
+    ## 80% of productes have color space transformation
+
+    colorSpaceImage = brightImage.copy()
+    if random.randint(1,100) > 20: 
+
+        r_value = random.randint(-100, 100)
+        g_value = random.randint(-100, 100)
+        b_value = random.randint(-100, 100)
+
+        for h in range(colorSpaceImage.size[0]):
+            for w in range(colorSpaceImage.size[1]):
+                r, g, b, a = colorSpaceImage.getpixel((h, w))
+                if r + g + b > 50:
+                    colorSpaceImage.putpixel((h,w),(r + r_value, g + g_value , b + b_value))
+
+    colorSpace_list.append(colorSpaceImage)
+
+    return colorSpace_list
+
+
+def write_to_label_file(outPath, width, height, x_coord, y_coord):
+    i = 0
+    
+    for i in range(len(x_coord)):
+        if x_coord[i] != 0 and y_coord[i] != 0:
+            x_center = (width[i]/2) + x_coord[i]
+            y_center = (height[i]/2) + y_coord[i]
+                
+            val1 = round(x_center/1920, 4)
+            val2 = round(y_center/1440, 4)
+            val3 = round(width[i]/1920, 4)
+            val4 = round(height[i]/1440, 4)
+
+            val1 = format(val1, '.4f')
+            val2 = format(val2, '.4f')
+            val3 = format(val3, '.4f')
+            val4 = format(val4, '.4f')
+
+            cam = imagePath.split(".")
+
+            with open(f"{outPath}/0_%d_{cam[0]}.txt" % count, "a") as f:
+                f.write(f"1 {val1} {val2} {val3} {val4}\n")
+
+
+def set_outPath(dataset):
+    if cam1 in inputPath:
+        outPath = f'{dir_tool}/{dir_mp}/vid/{dataset + "_" + current_time}/{cam1}'
+    elif cam2 in inputPath:
+        outPath = f'{dir_tool}/{dir_mp}/vid/{dataset + "_" + current_time}/{cam2}'
+    elif cam3 in inputPath:
+        outPath = f'{dir_tool}/{dir_mp}/vid/{dataset + "_" + current_time}/{cam3}'
+    elif cam4 in inputPath:
+        outPath = f'{dir_tool}/{dir_mp}/vid/{dataset + "_" + current_time}/{cam4}'
+    elif cam5 in inputPath:
+        outPath = f'{dir_tool}/{dir_mp}/vid/{dataset + "_" + current_time}/{cam5}'
+    elif cam6 in inputPath:
+        outPath = f'{dir_tool}/{dir_mp}/vid/{dataset + "_" + current_time}/{cam6}'
+
+    return outPath
+
+
+def save_image(list, imgPath, random_x_list, random_y_list):
+    manipulatedBG = imgBG.copy()
+    i = 0
+    for img in list:
+        manipulatedBG.paste(img, (random_x_list[i],random_y_list[i]), mask = img)
+        i += 1
+    manipulatedBG.save(imgPath)
+
+
+create_directories()
+
+for i in range(int(vid_number)):
+    random_video = random.choice(vid_list)
+
+    # selects random_video but not double
+    if random_video in used_vid:
+        while random_video in used_vid:
+            random_video = random.choice(vid_list)
+        used_vid.append(random_video)
+    else:
+        used_vid.append(random_video)
+
+    print(random_video)
+    BGPath = glob.glob(f'{parent_dir_mp}/{dir_all_vid}/{random_video}/frames/*png')
+    sortedBGPath = natsorted(BGPath)
+
+    # each frame of the random video
+    for video_frame in sortedBGPath:
+        inPath = f'{parent_dir_mp}/results/0600'
+
+        # saves frame in 223, than 224, than 225 ...
+        for imagePath in os.listdir(inPath):
+            imgBG = Image.open(video_frame).convert("RGBA")                                                                           
+
+            img_list = []
+            blurred_list = []
+            brightness_list = []
+            colorSpace_list = []       
+            random_x_list = []
+            random_y_list = []
+
+            orig_height_list = []
+            orig_width_list = []
+
+            # puts random amount of products between 4 and 8 into the image  
+            for p in range(random.randint(4,8)):                                                                            
+                random_product = random.choice(product_list)
+                inPath = f'{parent_dir_mp}/results/{random_product}'
+
+                inputPath = os.path.join(inPath, imagePath)
+                cam_nb = imagePath.split(".")
+                img = Image.open(inputPath)
+
+                # function to rotate the product
+                cropped_img_rotated, random_x, random_y = product_rotation()
+
+                img_list.append(cropped_img_rotated)
+                random_x_list.append(random_x)
+                random_y_list.append(random_y)
+
+                if img_manipulation:   
+                    blur(blurred_list)                                                                                                 ## add new function
+                    #brightness(brightness_list)
+
+
+            for x in img_list:
+                orig_width, orig_height = x.size
+                orig_width_list.append(orig_width)
+                orig_height_list.append(orig_height)
+            
+
+            orig_x_list = copy.deepcopy(random_x_list)
+            orig_y_list = copy.deepcopy(random_y_list)
+
+
+            if img_manipulation:
+                for x in manipulation:
+                    outPath = set_outPath(x)
+                    imgPath = os.path.join(outPath,f"0_%d_{imagePath}" % count)
+                    match x:                                                                                                                ## add case
+                        case "final":
+                            save_image(colorSpace_list, imgPath, orig_x_list, orig_y_list)
+                        case "origin":
+                            save_image(img_list, imgPath, orig_x_list, orig_y_list)
+                        
+                    write_to_label_file(outPath, orig_width_list, orig_height_list, orig_x_list, orig_y_list) 
+            else:
+                outPath = set_outPath("origin")
+                imgPath = os.path.join(outPath,f"0_%d_{imagePath}" % count)
+                save_image(img_list, imgPath, orig_x_list, orig_y_list)
+
+        count += 1
+
---
+++ recording_tool/exp8_generate_dataset.py
@@ -0,0 +1,137 @@
+import os,os.path
+import shutil
+import glob
+from natsort import natsorted
+import random
+
+dir_tool = "D:\\Datasets\\Experimentreihe_8" 
+dir = "origin"
+#dir = "final"
+
+test_input = input ("Anzahl der Ordner: ")
+new_folder = input("Name des Datensatzes: ")
+
+destination_folder = f'{dir_tool}/'
+png = 'png'
+txt = 'txt'
+images = 'images\\'
+labels = 'labels\\'
+dataset_path = destination_folder + new_folder
+
+train = 'train\\'
+val = 'val\\'
+test = 'test\\'
+
+
+os.mkdir(dataset_path)
+
+os.mkdir(destination_folder + train)
+os.mkdir(destination_folder + train + images)
+os.mkdir(destination_folder + train + labels)
+
+os.mkdir(destination_folder + val)
+os.mkdir(destination_folder + val + images)
+os.mkdir(destination_folder + val + labels)
+
+os.mkdir(destination_folder + test)
+os.mkdir(destination_folder + test + images)
+os.mkdir(destination_folder + test + labels)
+
+file_number = 0
+first_folder = 0
+for i in range(int(test_input)):
+    folder = input("    >>> Ordner Name: ")
+    recording_tool = glob.glob(f'{dir_tool}\\{dir}\\{folder}\\**/')
+    for folder in recording_tool:
+        for x in os.listdir(folder):
+            if os.path.isfile(os.path.join(folder, x)):
+                file_number += 1
+
+    png_or_txt = (file_number/2)
+    file_number = 0
+    
+    train_percentage = png_or_txt * 1.0
+    val_percentage = png_or_txt * 0.0
+    test_percentage = png_or_txt * 0.0
+
+    count = 0
+    for folder in recording_tool:
+        natsorted(folder)
+        for file in os.listdir(folder):
+            if png_or_txt > 0:
+                source = folder + file
+
+                if png in source:
+                    if os.path.isfile(source):
+                        if first_folder != "0":
+                            destination = destination_folder + train + images + str(first_folder) + "_" + file
+                        else:
+                            destination = destination_folder + train + images + file
+                        shutil.copy(source, destination)
+                                        
+                if txt in source:
+                    if os.path.isfile(source):
+                        if first_folder != "0":
+                            destination = destination_folder + train + labels + str(first_folder) + "_" + file
+                        else:
+                            destination = destination_folder + train + labels + file
+                        shutil.copy(source, destination)
+                        png_or_txt -= 1
+                        count += 1
+    first_folder += 1
+    while  val_percentage > 0: 
+        for file in os.listdir(destination_folder + train + images):
+            rand_int = random.randint(0, 9)
+            filename = file.split(".")
+
+            source_img = destination_folder + train + images + file
+            source_labels = destination_folder + train + labels + filename[0] + ".txt"
+
+            destination_img = destination_folder + val + images + file          
+            destination_labels = destination_folder + val + labels + filename[0] + ".txt"
+
+            if rand_int == 9:
+                if png in source_img:
+                    if os.path.isfile(source_img):
+                        shutil.copy(source_img, destination_img)
+                        os.remove(destination_folder + train + images + file)
+                        val_percentage -= 1
+                
+                for txt_file in os.listdir(destination_folder + train + labels):
+                    if txt_file == filename[0] + ".txt":
+                            shutil.copy(source_labels, destination_labels)
+                            os.remove(destination_folder + train + labels + filename[0] + ".txt")
+            
+            if val_percentage == 0:
+                break
+
+    while test_percentage > 0:
+        for file in os.listdir(destination_folder + train + images):
+            rand_int = random.randint(0, 10)
+            filename = file.split(".")
+
+            source_img = destination_folder + train + images + file
+            source_labels = destination_folder + train + labels + filename[0] + ".txt"
+
+            destination_img = destination_folder + test + images + file
+            destination_labels = destination_folder + test + labels + filename[0] + ".txt"
+
+            if rand_int == 10:
+                if png in source_img:
+                    if os.path.isfile(source_img):
+                        shutil.copy(source_img, destination_img)
+                        os.remove(destination_folder + train + images + file)
+                        test_percentage -= 1
+                    
+                for txt_file in os.listdir(destination_folder + train + labels):
+                    if txt_file == filename[0] + ".txt":
+                        shutil.copy(source_labels, destination_labels)
+                        os.remove(destination_folder + train + labels + filename[0] + ".txt")
+
+            if test_percentage == 0:
+                break
+
+
+shutil.move(destination_folder + train, dataset_path)
+shutil.move(destination_folder + val, dataset_path)
+shutil.move(destination_folder + test, dataset_path)
